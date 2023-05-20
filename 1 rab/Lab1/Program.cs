a5842f55665b4a2c3d72e8d9965500f34661bfa2
Hikinari <118520166+Narek24IS@users.noreply.github.com>
2023-05-20 23:13:46 +0300

    Add files via upload

---
+++ 1 rab/Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*7 вариант
+ Вычислить гиперболический косинус двумя способами*/
+
+Console.Write("1.Использование встроенной функции Math.Cosh(): ");
+
+
+double x = 3.56; // аргумент гиперболического косинуса
+double result = Math.Cosh(x);
+
+Console.WriteLine(result);
+
+
+Console.Write("2.Использование формулы cosh(x) = (e ^ x + e ^ (-x)) / 2: ");
+
+
+x = 3.56; // аргумент гиперболического косинуса
+result = (Math.Exp(x) + Math.Exp(-x)) / 2;
+
+Console.WriteLine(result);
---
+++ 1 rab/Prak1/Program.cs
@@ -0,0 +1,5 @@
+﻿/*7 вариант*/
+double result = 2.6 * (3 - Math.Pow(1.8, 2)) / Math.Sqrt(15.3) - 7.25 +
+    Math.Pow(4.1, 2) + 0.3 * (13.6 - 8.5);
+
+Console.WriteLine(Math.Round(result, 2));
