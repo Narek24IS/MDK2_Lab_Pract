f808f4a6430dbfbb4bb92a1a7f786d98b92c1fa4
Hikinari <118520166+Narek24IS@users.noreply.github.com>
2023-05-20 23:12:38 +0300

    Add files via upload

---
+++ 2 rab/Lab 2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*7 вариант
+Вводится символ арифметического оператора. Ответить его названием.*/
+char op = '\0';
+
+while (op != 'q')
+{
+    Console.Write("Введите знак оператора:");
+    op = Console.ReadKey().KeyChar;
+    Console.WriteLine();
+    switch (op)
+    {
+        case '=':
+            Console.WriteLine("Равно");
+            break;
+        case '+':
+            Console.WriteLine("Плюс");
+            break;
+        case '-':
+            Console.WriteLine("Минус");
+            break;
+        case '*':
+            Console.WriteLine("Умножение");
+            break;
+        case '/':
+            Console.WriteLine("Деление");
+            break;
+        default:
+            Console.WriteLine("Не арифметическая операция!");
+            break;
+    }
+}
