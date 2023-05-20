5a2f67d55d58b7f7f14ed2c8ecd40b3a3e24bafd
Hikinari <118520166+Narek24IS@users.noreply.github.com>
2023-05-20 23:07:50 +0300

    Add files via upload

---
+++ 6 rab/Program.cs
@@ -0,0 +1,17 @@
+﻿StreamReader file = new StreamReader("check.txt");
+double totalCost = 0;
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+    string[] elements = line.Split(' ');
+
+    int quantity = Convert.ToInt32(elements[2]);
+    double price = Convert.ToDouble(elements[3]);
+
+    totalCost += quantity * price;
+}
+
+file.Close();
+Console.WriteLine("Общая стоимость покупки: " + totalCost);
+Console.ReadLine();
